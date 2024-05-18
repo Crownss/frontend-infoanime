658d557abe2296e1aab794adee03e258e0513522
Crownss <djezen.ramsey@etb.co.id>
2024-05-19 00:24:27 +0700

    #64990 Deployed 🚀

---
+++ out/_next/static/chunks/pages/_app-b8beba70dfa3e9aa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{8491:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(1484)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(8491),_(3632)}),_N_E=n.O()}]);
